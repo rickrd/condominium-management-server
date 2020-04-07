3051dfb5824fe69109bdadb94c93e4064e651f37
rickrd <desouzaricardoc@gmail.com>
2020-04-07 17:15:47 -0300

    Added mongoose

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 import apartmentRouter from './routes/apartment'
+
+const {
+  env: { MONGODB_PASSWORD },
+} = process
+
+mongoose.connect(`mongodb+srv://admin:<${MONGODB_PASSWORD}>@condominium-management-heifk.gcp.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const server = express()
 
@@ -8,7 +15,6 @@ server.use(express.json())
 
 server.use('/apartments', apartmentRouter)
 
-const listener = server.listen(8000, function() {
+const listener = server.listen(8000, function () {
   console.log('Listening on port ' + listener.address().port)
 })
-
